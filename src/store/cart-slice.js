d37fa88413f102bc4b80897fbeabbf4405bb45d0
ashishsharma157 <sharma.ashish78@gmail.com>
2024-04-19 15:02:31 -0700

    complete redux action

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -8,18 +8,38 @@ const cartSlice=createSlice({
     },
     reducers:{
         addItemToCart(state, action){
+            console.log("slice");
             const newItem=action.payload;
+            console.log(newItem);
             const existingItem=state.items.find(item=>item.id===newItem.id);
+            state.totalQuanitity++;
             if(!existingItem)
             {
-                state.items.push({id:newItem.id, price:newItem.price, quantity:1, totalPrice:newItem.price, name:newItem.name});
+                state.items.push({id:newItem.id, price:newItem.price, quantity:1, totalPrice:newItem.price, name:newItem.title});
             }
             else
             {
                 existingItem.quantity++;
                 existingItem.totalPrice=existingItem.totalPrice+newItem.price
             }
+            //console.log(state.items[0]);
         },
-        removeItemToCart(){}
+        removeItemToCart(state, action){
+            const id=action.payload;
+            const existingItem =state.items.find(item=>item.id===id);
+            state.totalQuanitity--;
+            if(existingItem.quantity===1)
+            {
+                state.items=state.items.filter(item=>item.id !== id);
+            }
+            else
+            {
+                existingItem.quantity--;
+                existingItem.totalPrice=existingItem.totalPrice-existingItem.price;
+            }
+        }
     }
-})
+});
+
+export const cartActions=cartSlice.actions;
+export default cartSlice;
